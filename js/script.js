a3ef05fe0aac201447c9b417f75c15afa6a7c44c
Jordan Hu <jhu@Jordans-MBP.home>
2015-10-21 00:13:33 -0400

    interview with annie
    annie atkins

---
+++ js/script.js
@@ -0,0 +1,102 @@
+$(document).ready(function() {
+
+$(".pink, .example").hide();
+
+$(".original").hover(function() {
+	$(".pink").show()
+	$(".example").fadeIn(500);
+
+});
+
+$(".pink2, .example2").hide();
+
+$(".original2").hover(function() {
+	$(".pink2").show()
+	$(".example2").fadeIn(500);
+
+});
+
+$(".pink3, .example3").hide();
+
+$(".original3").hover(function() {
+	$(".pink3").show()
+	$(".example3").fadeIn(500);
+
+});
+
+$(".sign2, .example4").hide();
+
+$(".sign1").hover(function() {
+	$(".sign2").show()
+	$(".example4").fadeIn(500);
+
+});
+
+$(".book2, .example4").hide();
+
+$(".book1").hover(function() {
+	$(".book2").show()
+	$(".example4").fadeIn(500);
+
+});
+
+$(".newspaper2, .example4").hide();
+
+$(".newspaper1").hover(function() {
+	$(".newspaper2").show()
+	$(".example4").fadeIn(500);
+
+});
+
+$(".vintage2, .example5").hide();
+
+$(".vintage1").hover(function() {
+	$(".vintage2").show()
+	$(".example5").fadeIn(500);
+
+});
+
+$(".vintage4, .example6").hide();
+
+$(".vintage3").hover(function() {
+	$(".vintage4").show()
+	$(".example6").fadeIn(500);
+
+});
+
+$(".vintage6, .example7").hide();
+
+$(".vintage5").hover(function() {
+	$(".vintage6").show()
+	$(".example7").fadeIn(500);
+
+});
+
+$(".book2, .example8").hide();
+
+$(".book1").hover(function() {
+	$(".book2").show()
+	$(".example8").fadeIn(500);
+
+});
+
+$(".newspaper2, .example9").hide();
+
+$(".newspaper1").hover(function() {
+	$(".newspaper2").show()
+	$(".example9").fadeIn(500);
+
+});
+
+// text appear
+// $(".original").hover(function() {
+// 	$(".original").hide();
+// 	$(".pink").show();
+// 	$(".example").fadeIn(500);
+
+// $(".original2").hover(function() {
+// 	$(".original2").hide();
+// 	$(".pink2").show();
+// 	$(".example").fadeIn(500);
+
+});
